7a544f15dfbd03372b928cab1e6358345d3eb842
Jhon Mario Argumedo <jmario1233@hotmail.com>
2025-10-03 17:41:55 -0500

    Initial commit

---
+++ src/main/java/com/backend_gestion_biblioteca/backend_gestion_biblioteca/BackendGestionBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.backend_gestion_biblioteca.backend_gestion_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend_gestion_biblioteca/backend_gestion_biblioteca/BackendGestionBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_gestion_biblioteca.backend_gestion_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
